a2047d97531f3092fa21353d830a2e2057f98ae8
Raquel Azcue <rvazcue@gmail.com>
2018-06-21 22:02:45 -0500

    updating files

--- signInPage.js
+++ signInPage.js
@@ -10,23 +10,103 @@ var config = {
   firebase.initializeApp(config);
 var database = firebase.database();
 
-$("#signInBtn").on("click", function (event) {
-    event.preventDefault();
+//This snippet was taken directly from the Firebase documentation
+//It is an observer and it checks to see if a user is currently 
+//logged into the app
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
 
-   console.log($("#emailInput").val())
+        var user = firebase.auth().currentUser
+        //The snippet below checks to see if the user's email is verified or not
+        if (user.emailVerified) {
+            console.log('Email is verified');
+          }
+          else {
+            console.log('Email is not verified');
+          }
+      // If user is signed in:
+      //The login-form should be hidden
+      //$("#formEl").hide()
 
-})
+      //this line also needs to be hidden until I figure out how
+      //to send link for verification to email
+      //$(".loggedin-Div").show()
+
+          if (user != null){
+            //This snippet display the welcome message!! 
+            //so I will block this out until it is needed
+                    // var email =  $("#emailInput").val()
+                    // $("#userWelcomeEmail").text(email)
+                    
+                 }
+
+    } else {
+      // No user is signed in.
+    }
+  });
+  
+
+
 
 $("#createLoginBtn").on("click", function (event) {
     event.preventDefault();
-
     $("#signInBtn").hide()
     $("#createLoginBtn").text("Submit")
     $(".showUp").show()
 
-    
-    //create a 'username Input box'
+})
 
+$("#signInBtn").on("click", function (event) {
+    event.preventDefault();
+
+ var email =  $("#emailInput").val()
+ var password =  $("#passwordInput").val()
+
+ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var newLine = $("<br>")
+    // ...
+    window.alert("Error: " + errorMessage + newLine +"Error Code: " + errorCode) });
+
+})
+
+$("#logoutBtn").on("click", function(){
+    console.log('jdgvowach')
+    firebase.auth().signOut()
+    console.log('jdgvowach')
+    //This snippet goes after the 'signOut()' and can be uesd to display a good-by message
+    // .then(function() {
+    //     // Sign-out successful.
+    //   }).catch(function(error) {
+    //     // An error happened.
+      //});
+})
+
+$("#createLoginBtn").on("click", function(){
+
+    //grab the email and password values
+    var email =  $("#emailInput").val()
+    var password =  $("#passwordInput").val()
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message; 
+        //Throw an alert if we 'catch' an error during the sign-up process
+        window.alert("Error: " + errorMessage + newLine +"Error Code: " + errorCode)})
+        
+        $("#createLoginBtn").hide()
+        $("#pwLabel").hide()
+        $("#passwordInput").hide()
+        $("#verifyEmail").show()
+        $("#emailInput").val('')
+        $("#sendEmailVerification").show()
+
+})
+//Code for requesting a verification email goes below:
+$("#sendEmailVerification").on("click", function(){
 
 
 })
