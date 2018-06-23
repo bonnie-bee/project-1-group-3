af99db85645ce4e9fe1bfc4a94a237812077a002
Wilson P <wilson.prevette@gmail.com>
2018-06-23 00:01:57 -0500

    customer search bar now shows markers on map

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,78 +1,27 @@
-function initAutocomplete() {
+function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -33.8688, lng: 151.2195 },
-        zoom: 13,
-        mapTypeId: 'roadmap'
+      zoom: 8,
+      center: {lat: -34.397, lng: 150.644}
     });
+    var geocoder = new google.maps.Geocoder();
 
-    var input = document.getElementById('pac-input');
-
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    $("#search-input").keypress(function () {
-        $("#pac-input").val($(this).val());
-    });
-
-    var mySearch = new google.maps.places.SearchBox(document.getElementById('search-input'));
-    var mySearchButton = document.getElementById("searchBtn").onclick = function(event) {
+    document.getElementById('searchBtn').addEventListener('click', function(event) {
         event.preventDefault();
-        searchBox.getPlaces();
-    }
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
+      geocodeAddress(geocoder, map);
     });
+  }
 
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-
-    searchBox.addListener('places_changed', function () {
-        var places = searchBox.getPlaces();
-
-        if (places.length == 0) {
-            return;
-        }
-
-        // Clear out the old markers.
-        markers.forEach(function (marker) {
-            marker.setMap(null);
+  function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('search-input').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
         });
-        markers = [];
-
-        // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
-        places.forEach(function (place) {
-            if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-
-            var icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-                map: map,
-                icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
     });
-}
+  }
