fa910f71cca6f5385fcc33781fa720eeaf3fc520
Wilson P <wilson.prevette@gmail.com>
2018-06-27 12:33:14 -0500

    changed placement of login

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # project-1-group-3
 
-* Project Manager: Raquel Azcue
-* Git Master: Bonnie Birch
-* Team: Andrew Duong, Wilson Prevette
+Project Manager: Raquel Azcue
+
+Git Master: Bonnie Birch
+
+## Proposal
+
+App Name: __FreshAir App__
+
+[App sketch](Project1Sketch.jpg)
+
+Group 3 would like to create a map that searches for nearby parks and pulls the weather in that area.
+
+The user would search for parks based on their location, then the app would contact the Google Maps API to find a list of parks and their ratings. Once the user selects the park, the page expands to only show that park. The weather API will be triggered when the park is selected and the weather for that location will be displayed on the new page. The app would also have a comment section on the same park page where reviews and comments of the park can be made!
+
+__Branches/ Tasks Breakdown:__
+
+Design/Forms:  Wilson 
+* implementing the design we have sketched out
+
+Weather API : Bonnie
+* building AJAX requests and queries for the Weather API 
+* send out Slack message to group members when the repository is updated
+
+Google Maps: Andrew
+* building AJAX requests and queries for the Google Maps API
+
+Comments/Reviews: Raquel 
+* creating comments sections/review sections
+    * sections will require users to enter email in order to post reviews/comments.
+
+__APIs:__
+   * Weather API (the one from class)
+   * Google Maps API
+   
+   
---
+++ assets/js/commentsJS.js
@@ -0,0 +1,64 @@
+var config = {
+    apiKey: "AIzaSyDxPAAEU9IpAM2xXpNOzL9LUlSJh3cN9jw",
+    authDomain: "freshairp1-64c68.firebaseapp.com",
+    databaseURL: "https://freshairp1-64c68.firebaseio.com",
+    projectId: "freshairp1-64c68",
+    storageBucket: "freshairp1-64c68.appspot.com",
+    messagingSenderId: "456240610175"
+  };
+  firebase.initializeApp(config);
+var database = firebase.database();
+
+//Programming the comment box to appear only after a park has been selected 
+//Using a generic HTML button to simulate selection of park
+    $("#selectParkBtn").on("click", function (event) {
+         event.preventDefault();
+
+        $("#selectParkBtn").hide()
+
+        var cmtBoxContDiv = $("<div>").attr("id", "cmtBoxContDiv")
+        cmtBoxContDiv.addClass("container-fluid")
+        cmtBoxContDiv.text("Comments:")
+
+        var commentSubmitBtn = $('<button id="commentSubmitBtn">Submit</button>')
+
+        var inputBox = $('<textarea type="text" placeholder="Comment here..." id="commentInput">')
+
+        $("#divBody").append(cmtBoxContDiv)
+        $("#divBody").append(inputBox)
+        $("#divBody").append($("<div id='starDiv'>"))
+        for (x =1; x<= 5 ; x++){
+            let icon = $('<span class="icon"></span>') 
+            // let starNum = "star" + x
+            // let label = $("<label class='labelEls'>").attr("id", starNum)
+           $("#starDiv").append("<input class='star' id='radioBtn' type='radio'>",icon)
+            }
+
+        $("#divBody").append($("<div>").append(commentSubmitBtn)) 
+
+        $(document).on("click", "#commentSubmitBtn", function (event) {
+            event.preventDefault();
+            var cmt = $("#commentInput").val()
+            var today = new Date();
+            var readableTime =  today.toDateString()
+           console.log(cmt)  
+           console.log(readableTime) 
+            //send this message to firebase......
+           //how do i create multiple firebase objects without them
+           //rewriting over one-another???
+           database.ref().child('test').push({
+            comment: cmt,
+            time: readableTime
+        })
+            //now paste the comment on the comments wall!
+            $("#cmtBoxContDiv").append($('<div class="commentz">').append(cmt))
+            //for some reason the submit button only works once
+            //a second click results in the app crashing......
+        })
+        database.ref().on("value", function(snapshot) {
+        console.log(snapshot.val())
+        })
+
+    })
+
+    
